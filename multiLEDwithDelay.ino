8815fe1ab1b848148cd52d7a66ea5fffd42429a6
MadhuKashyap <40714383+MadhuKashyap@users.noreply.github.com>
2019-05-11 00:46:45 +0530

    Create multiLEDwithDelay.ino

---
+++ multiLEDwithDelay.ino
@@ -0,0 +1,23 @@
+/***********MULTIPLE LEDs WITH DELAY*************/
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(2,HIGH);
+delay(50);
+digitalWrite(2,LOW);
+delay(10);
+digitalWrite(3,HIGH);
+delay(50);
+digitalWrite(3,LOW);
+delay(10);
+digitalWrite(4,HIGH);
+delay(50);
+digitalWrite(4,LOW);
+delay(10);
+}
